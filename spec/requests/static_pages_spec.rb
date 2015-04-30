56ad43720b00d02829fdf795cc9cd705ef351294
Alex223124 <vzadnicy@yandex.ru>
2015-04-30 21:04:45 +0300

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 describe "Static pages" do
 describe "Home page" do
 it "Should have the content 'App3'" do
-	visit '/static_pages/home'
+	visit root_path
 	expect(page).to have_content('App3')
   end
  it "Should have the title 'App3'" do
-	visit '/static_pages/home'
+	visit root_path
 	expect(page).to have_title("Ruby on Rails Tutorial App3 | Home")
   end
  end
@@ -15,11 +15,11 @@ it "Should have the content 'App3'" do
 
  describe "Help page" do
  it "should have the content 'Help'" do
- 	visit '/static_pages/help'
+ 	visit help_path
  	expect(page).to have_content('Help')
  end
  it "Should have the title 'Help'" do
-	visit '/static_pages/help'
+	visit help_path
 	expect(page).to have_title("Ruby on Rails Tutorial App3 | Help")
   end
  end
@@ -27,11 +27,11 @@ it "Should have the content 'App3'" do
 
  describe "About page" do
  it "Should have the content 'About Us'" do
- 	visit '/static_pages/about'
+ 	visit about_path
  	expect(page).to have_content('About Us')
  end
  it "Should have the title 'About Us'" do
-	visit '/static_pages/about'
+	visit about_path
 	expect(page).to have_title("Ruby on Rails Tutorial App3 | About Us")
   end
  end
